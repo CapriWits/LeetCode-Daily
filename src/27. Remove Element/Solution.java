d734d31c545061ea843fb7843b60eb5f16b01b28
hypocrite30 <1027093394@qq.com>
2021-04-19 11:48:12 +0800

    add 27

---
+++ src/27. Remove Element/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+
+    public int removeElement(int[] nums, int val) {
+        int idx = 0;
+        for (int num : nums) {
+            if (num != val) nums[idx++] = num;
+        }
+        return idx;
+    }
+
+}
+
