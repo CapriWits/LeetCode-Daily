6e47b6d195856899ef322eab27e26fea149e89f5
hypocrite30 <1027093394@qq.com>
2021-05-03 11:19:11 +0800

    add 7

---
+++ src/7. Reverse Integer/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+
+    public int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            int temp = res * 10 + x % 10;
+            if (temp / 10 != res)
+                return 0;
+            res = temp;
+            x /= 10;
+        }
+        return res;
+    }
+
+}
+
