c1a469668c9ca0c78474615f540fb73ef74e35de
hypocrite30 <1027093394@qq.com>
2021-05-07 19:40:37 +0800

    add 1486

---
+++ src/1486. XOR Operation in an Array/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int xorOperation(int n, int start) {
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res ^= (start + i * 2);
+        }
+        return res;
+    }
+}
+
