2c75dba0154164e1819ad4d3c23e166a67e69ebd
hypocrite30 <1027093394@qq.com>
2021-05-22 09:06:01 +0800

    add 810

---
+++ src/810. Chalkboard XOR Game/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+/* 810. Chalkboard XOR Game */
+public class Solution {
+    public boolean xorGame(int[] nums) {
+        if (nums.length % 2 == 0) {
+            return true;
+        }
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+        return xor == 0;
+    }
+}
