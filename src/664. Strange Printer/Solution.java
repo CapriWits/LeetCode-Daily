ff23e5713ca0f34d7541695adf40c583427b4243
hypocrite30 <1027093394@qq.com>
2021-05-24 15:56:22 +0800

    add 664

---
+++ src/664. Strange Printer/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+/* 664. Strange Printer */
+public class Solution {
+
+    public int strangePrinter(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i][j - 1];
+                } else {
+                    int minVal = Integer.MAX_VALUE;
+                    for (int k = i; k < j; k++) {
+                        minVal = Math.min(minVal, dp[i][k] + dp[k + 1][j]);
+                    }
+                    dp[i][j] = minVal;
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+
+}
