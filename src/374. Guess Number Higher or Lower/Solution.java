fcb0980b99cbbb3b60e30fb4a2a0212338bd5e57
hypocrite30 <1027093394@qq.com>
2021-06-14 10:48:28 +0800

    feat: add 374

---
+++ src/374. Guess Number Higher or Lower/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+/* 374. Guess Number Higher or Lower */
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (guess(mid) <= 0) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
