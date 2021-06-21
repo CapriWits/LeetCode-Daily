ca4d46bb81332f9a4982b40e9036fa3da12f552e
hypocrite30 <1027093394@qq.com>
2021-06-21 10:28:17 +0800

    feat: add 401

---
+++ src/401. Binary Watch/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+/* 401. Binary Watch */
+public class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> ans = new ArrayList<String>();
+        for (int h = 0; h < 12; ++h) {
+            for (int m = 0; m < 60; ++m) {
+                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+                    ans.add(h + ":" + (m < 10 ? "0" : "") + m);
+                }
+            }
+        }
+        return ans;
+    }
+}
