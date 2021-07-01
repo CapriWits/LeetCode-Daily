19b9c4f142c3a62cf8ae4de2e405f4d285e80c93
hypocrite30 <1027093394@qq.com>
2021-07-01 20:47:55 +0800

    feat: add LCP 07

---
+++ src/LCP 07. 传递信息/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/* LCP 07. 传递信息 */
+public class Solution {
+    public int numWays(int n, int[][] relation, int k) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 0; i < k; i++) {
+            int[] next = new int[n];
+            for (int[] edge : relation) {
+                int src = edge[0], dst = edge[1];
+                next[dst] += dp[src];
+            }
+            dp = next;
+        }
+        return dp[n - 1];
+    }
+}
