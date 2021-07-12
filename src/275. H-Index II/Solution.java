13d9fa7cdbf204bde4ac2fab8709e336aabf2602
hypocrite30 <1027093394@qq.com>
2021-07-12 08:47:37 +0800

    feat: add 275

---
+++ src/275. H-Index II/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/* 275. H-Index II */
+public class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (citations[mid] >= n - mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return n - left;
+    }
+}
