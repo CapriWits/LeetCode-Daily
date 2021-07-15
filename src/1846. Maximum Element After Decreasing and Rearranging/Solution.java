a37f79f8fb249b4b1c8610faf0db1e518dc7a215
hypocrite30 <1027093394@qq.com>
2021-07-15 10:00:16 +0800

    feat: add 1846

---
+++ src/1846. Maximum Element After Decreasing and Rearranging/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+/* 1846. Maximum Element After Decreasing and Rearranging */
+public class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        arr[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            arr[i] = Math.min(arr[i], arr[i - 1] + 1);
+        }
+        return arr[n - 1];
+    }
+}
