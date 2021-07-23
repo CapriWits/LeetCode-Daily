5d8328a4112f2b987a5a709b84fc39d8e25e234d
hypocrite30 <1027093394@qq.com>
2021-07-23 23:38:53 +0800

    feat: add 1893

---
+++ src/1893. Check if All the Integers in a Range Are Covered/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+/* 1893. Check if All the Integers in a Range Are Covered */
+public class Solution {
+    public boolean isCovered(int[][] ranges, int left, int right) {
+        int[] diff = new int[52];   // 差分数组
+        for (int[] range : ranges) {
+            ++diff[range[0]];
+            --diff[range[1] + 1];
+        }
+        // 前缀和
+        int curr = 0;
+        for (int i = 1; i <= 50; ++i) {
+            curr += diff[i];
+            if (i >= left && i <= right && curr <= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
