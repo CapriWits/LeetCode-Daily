e9de82fb6b9eca05e852da4f078543895fb3040c
hypocrite30 <1027093394@qq.com>
2021-08-09 12:20:15 +0800

    feat: add 313

---
+++ src/313. Super Ugly Number/Solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+/* 313. Super Ugly Number */
+public class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        Set<Long> seen = new HashSet<Long>();
+        PriorityQueue<Long> heap = new PriorityQueue<Long>();
+        seen.add(1L);
+        heap.offer(1L);
+        int ugly = 0;
+        for (int i = 0; i < n; i++) {
+            long curr = heap.poll();
+            ugly = (int) curr;
+            for (int prime : primes) {
+                long next = curr * prime;
+                if (seen.add(next)) {
+                    heap.offer(next);
+                }
+            }
+        }
+        return ugly;
+    }
+}
