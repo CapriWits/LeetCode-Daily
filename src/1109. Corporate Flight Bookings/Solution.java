6d9160c2331be9dae13790c0b8dabbe3cfe7e439
hypocrite30 <1027093394@qq.com>
2021-08-31 15:16:00 +0800

    feat: add 1109

---
+++ src/1109. Corporate Flight Bookings/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/* 1109. Corporate Flight Bookings */
+public class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] nums = new int[n];
+        for (int[] booking : bookings) {
+            nums[booking[0] - 1] += booking[2];
+            if (booking[1] < n) {
+                nums[booking[1]] -= booking[2];
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+}
