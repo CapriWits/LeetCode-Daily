c6908c81af4f50194215d34e7d34945169e31d3a
hypocrite30 <1027093394@qq.com>
2021-09-02 10:36:18 +0800

    feat: update src

---
+++ src/剑指 Offer 22. 链表中倒数第k个节点/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+/* 剑指 Offer 22. 链表中倒数第k个节点 */
+public class Solution {
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode former = head, latter = head;
+        for(int i = 0; i < k; i++)
+            former = former.next;
+        while(former != null) {
+            former = former.next;
+            latter = latter.next;
+        }
+        return latter;
+    }
+}
