767d6c797ebb23c9621ccf5593e1d58a95dc906f
hypocrite30 <1027093394@qq.com>
2021-09-06 21:11:49 +0800

    feat: add 704

---
+++ src/704. Binary Search/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+/* 704. Binary Search */
+public class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int num = nums[mid];
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
