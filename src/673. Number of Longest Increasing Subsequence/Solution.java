58061372e67572dd01a103fb98248fffef9adf8f
hypocrite30 <1027093394@qq.com>
2021-09-20 11:51:44 +0800

    feat: add 673

---
+++ src/673. Number of Longest Increasing Subsequence/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/* 673. Number of Longest Increasing Subsequence */
+public class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] f = new int[n], g = new int[n];
+        int max = 1;
+        for (int i = 0; i < n; i++) {
+            f[i] = g[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    if (f[i] < f[j] + 1) {
+                        f[i] = f[j] + 1;
+                        g[i] = g[j];
+                    } else if (f[i] == f[j] + 1) {
+                        g[i] += g[j];
+                    }
+                }
+            }
+            max = Math.max(max, f[i]);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (f[i] == max) ans += g[i];
+        }
+        return ans;
+    }
+}
