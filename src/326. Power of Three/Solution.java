49ca8cddaa4416fd5e56998f1e40b686cd2de67b
hypocrite30 <1027093394@qq.com>
2021-09-23 10:37:34 +0800

    feat: add 326

---
+++ src/326. Power of Three/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+/* 326. Power of Three */
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        while (n != 0 && n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
