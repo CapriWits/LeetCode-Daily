dbb1f703957c385596667b28e570a1012e7d4d7e
hypocrite30 <1027093394@qq.com>
2021-09-29 15:16:57 +0800

    feat: add 517

---
+++ src/517. Super Washing Machines/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+/* 517. Super Washing Machines */
+public class Solution {
+    public int findMinMoves(int[] machines) {
+        int tot = Arrays.stream(machines).sum();
+        int n = machines.length;
+        if (tot % n != 0) {
+            return -1;
+        }
+        int avg = tot / n;
+        int ans = 0, sum = 0;
+        for (int num : machines) {
+            num -= avg;
+            sum += num;
+            ans = Math.max(ans, Math.max(Math.abs(sum), num));
+        }
+        return ans;
+    }
+}
