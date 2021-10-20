2f36dd183e0ab771dcc38289b1cfc218e550b325
hypocrite30 <1027093394@qq.com>
2021-10-20 11:18:56 +0800

    feat: add 453

---
+++ src/453. Minimum Moves to Equal Array Elements/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+/* 453. Minimum Moves to Equal Array Elements */
+public class Solution {
+    public int minMoves(int[] nums) {
+        int min = Arrays.stream(nums).min().getAsInt();
+        int res = 0;
+        for (int num : nums) {
+            res += num - min;
+        }
+        return res;
+    }
+}
