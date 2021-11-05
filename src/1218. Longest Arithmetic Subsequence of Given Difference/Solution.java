f2e841efc72d474785f432c0e202cd9049764eee
hypocrite30 <1027093394@qq.com>
2021-11-05 10:39:52 +0800

    feat: add 1218

---
+++ src/1218. Longest Arithmetic Subsequence of Given Difference/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+/* 1218. Longest Arithmetic Subsequence of Given Difference */
+public class Solution {
+    public int longestSubsequence(int[] arr, int difference) {
+        int ans = 0;
+        Map<Integer, Integer> dp = new HashMap<Integer, Integer>();
+        for (int v : arr) {
+            dp.put(v, dp.getOrDefault(v - difference, 0) + 1);
+            ans = Math.max(ans, dp.get(v));
+        }
+        return ans;
+    }
+}
