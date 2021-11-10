274d8061115be1710022485163db36d4da2b9b84
hypocrite30 <1027093394@qq.com>
2021-11-10 20:10:15 +0800

    feat: add 495

---
+++ src/495. Teemo Attacking/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/* 495. Teemo Attacking */
+public class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int ans = 0;
+        int expired = 0;
+        for (int i = 0; i < timeSeries.length; ++i) {
+            if (timeSeries[i] >= expired) {
+                ans += duration;
+            } else {
+                ans += timeSeries[i] + duration - expired;
+            }
+            expired = timeSeries[i] + duration;
+        }
+        return ans;
+    }
+}
