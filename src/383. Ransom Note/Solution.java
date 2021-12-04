b4892af3eef3cf72e37cbe795264f79942dd12a7
hypocrite30 <1027093394@qq.com>
2021-12-04 11:09:49 +0800

    feat: add 383

---
+++ src/383. Ransom Note/Solution.java
@@ -0,0 +1,13 @@
+/* 383. Ransom Note */
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] cnt = new int[26];
+        for (int i = 0; i < magazine.length(); i++)
+            cnt[magazine.charAt(i) - 'a']++;
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (cnt[ransomNote.charAt(i) - 'a'] <= 0) return false;
+            cnt[ransomNote.charAt(i) - 'a']--;
+        }
+        return true;
+    }
+}
