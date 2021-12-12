49f2a10fe65bce46fdff16b00f0a6ef499890f44
hypocrite30 <1027093394@qq.com>
2021-12-12 11:02:32 +0800

    feat: add 709

---
+++ src/709. To Lower Case/Solution.java
@@ -0,0 +1,12 @@
+/* 709. To Lower Case */
+public class Solution {
+    public String toLowerCase(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= 'A' && c <= 'Z') c |= 32;
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
