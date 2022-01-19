a92aa675981e5762004c4f4b6d4349e4692e96b8
hypocrite30 <1027093394@qq.com>
2022-01-19 10:09:33 +0800

    feat: add 219

---
+++ src/219. Contains Duplicate II/Solution.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/* 219. Contains Duplicate II */
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && Math.abs(map.get(nums[i]) - i) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
